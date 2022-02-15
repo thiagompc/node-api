48a9bfeaa815e6d8b13749a6e406fee8c0217284
Thiago <thiago099carvalho@gmail.com>
2022-02-15 01:05:17 -0300

    create server

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end('<h1>Hello world!<\h1>');
+});
+
+server.listen(3001, () => console.log('Server started at http://localhost:3001'));
