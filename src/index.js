5e7d3b89ec2f3424c2f2afa925846e899f9c5021
Thiago <thiago099carvalho@gmail.com>
2022-02-16 00:38:25 -0300

    create user controller/route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const http = require('http');
 
+const routes = require('./routes');
+
 const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end('<h1>Hello world!<\h1>');
+
+    const route = routes.find((routeObj) => (
+        routeObj.endpoint === request.url && routeObj.method === request.method
+    ));
+
+    if (route) {
+        route.handler(request, response);
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.end(`Cannot ${request.method} ${request.url}`);
+    }
 });
 
-server.listen(3001, () => console.log('Server started at http://localhost:3001'));
+server.listen(3002, () => console.log('Server started at http://localhost:3002'));
