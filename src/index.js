b1ca89a7145f1a6ba138c6927cf714e7a9fdf265
Thiago <thiago099carvalho@gmail.com>
2022-02-21 00:28:07 -0300

    update get query params method

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const http = require('http');
-const url = require('url');
+const {URL} = require('url');
 
 const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
 
-    const parsedUrl = url.parse(request.url, true);
+    // const parsedUrl = url.parse(request.url, true);
+
+    const parsedUrl = new URL(`http://localhost:3002${request.url}`);
 
     const route = routes.find((routeObj) => (
         routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
     ));
 
     if (route) {
-        request.query = parsedUrl.query;
+        request.query = Object.fromEntries(parsedUrl.searchParams);
         route.handler(request, response);
     } else {
         response.writeHead(404, { 'Content-Type': 'text/html' });
