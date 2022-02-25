49daf57888a7f1ef764ef1036315140ab6295b25
Thiago <thiago099carvalho@gmail.com>
2022-02-25 16:41:43 -0300

    added update and delete user routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const { URL } = require('url');
 
+const bodyParser = require('./helpers/bodyParser');
 const routes = require('./routes');
 
 const server = http.createServer((request, response) => { 
@@ -29,8 +30,12 @@ const server = http.createServer((request, response) => {
             response.writeHead(statusCode, { 'Content-Type': 'application/json' });
             response.end(JSON.stringify(body));
         };
-
-        route.handler(request, response);
+        if (['POST', 'PUT', 'PATCH'].includes(request.method)) {
+            bodyParser(request, () => route.handler(request, response));
+        }
+        else {
+            route.handler(request, response);
+        }
         
     } else {
         response.writeHead(404, { 'Content-Type': 'text/html' });
